e82cb1b8db5c580a2bdecec1ec95da21c5a148a2
Rakesh Arunachalam <rakesh@thoughtbot.com>
2024-08-23 13:27:17 +0100

    Post tweets to Supabase

--- db/posttweets.ts
+++ db/posttweets.ts
@@ -2,8 +2,23 @@ require("dotenv").config();
 
 import fs from "fs";
 import { TweetContentOutput } from "../src/types";
+import { supabase } from "./client";
 
 const output = fs.readFileSync("output.json", "utf-8");
-const tweets: TweetContentOutput[] = JSON.parse(output);
+const tweetAccounts: TweetContentOutput[] = JSON.parse(output);
 
-console.log(tweets.length);
+async function main() {
+  tweetAccounts.forEach(async ({ account, content }) => {
+    content.forEach(async (tweetContent) => {
+      const { statusText, error } = await supabase
+        .from("transfer_tweets")
+        .insert({
+          account,
+          original_news: tweetContent,
+        });
+      console.log(statusText, error);
+    });
+  });
+}
+
+main();
